e4cb32ea0bf2913836b243400c8506136dd7d26b
Merin <marziabintemerin@gmail.com>
2023-12-04 10:29:44 +0600

    Created Introductionary Pages.

--- lib/main.dart
+++ lib/main.dart
@@ -1,62 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:wellbeing_app/pages/introductionary_pages.dart';
 
-void main() => runApp(MaterialApp(
-  home: Home(),
-  ),
-);
+void main() => runApp( MyApp() );
 
-
-class Home extends StatelessWidget {
-  const Home({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold (
-      appBar: AppBar(
-        title: Text('Welcome To WellBeing'),
-        centerTitle: true,
-        backgroundColor: Colors.deepPurpleAccent[200] ,
-      ),
-      body: Row(
-        children: [
-          Expanded(
-            flex: 2,
-            child: Container(
-                  child: Text('One',textAlign: TextAlign.center, style: TextStyle(
-                      fontSize: 20 ),),
-                  padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 30.0),
-                  decoration: BoxDecoration(
-                    color: Colors.deepPurple[300],
-                  ),
-                ),
-          ),
-          Expanded(
-            flex: 2,
-            child: Container(
-              child: Text('Two', textAlign: TextAlign.center, style: TextStyle(
-                  fontSize: 20 ),),
-              padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 30.0),
-              decoration: BoxDecoration(
-                color: Colors.deepPurple[200],
-              ),
-            ),
-          ),
-          Expanded(
-            flex: 2,
-            child: Container(
-              child: Text('Three',textAlign: TextAlign.center, style: TextStyle(
-                fontSize: 20 ),),
-              padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 30.0),
-              decoration: BoxDecoration(
-                color: Colors.deepPurple[100],
-              ),
-            ),
-          ) ,
-        ],
-      ) ,
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Introductionary(),
 
-    ) ;
+    );
   }
 }
+
+
 
 
---
+++ lib/pages/introductionary_pages.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Introductionary extends StatelessWidget {
+  const Introductionary({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
