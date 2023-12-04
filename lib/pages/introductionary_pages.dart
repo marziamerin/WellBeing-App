f3e72b4f467b47a3d91f5718162358809987780c
Merin <marziabintemerin@gmail.com>
2023-12-04 13:26:19 +0600

    four pageview scrollable pages.

--- lib/pages/introductionary_pages.dart
+++ lib/pages/introductionary_pages.dart
@@ -1,10 +1,48 @@
 import 'package:flutter/material.dart';
 
-class Introductionary extends StatelessWidget {
+class Introductionary extends StatefulWidget {
   const Introductionary({super.key});
 
   @override
+  State<Introductionary> createState() => _IntroductionaryState();
+}
+
+class _IntroductionaryState extends State<Introductionary> {
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Stack(
+        children: [
+          PageView(
+            children: [
+              Container(
+                color: Colors.deepPurple[400],
+              ),
+              Container(
+                color: Colors.purple[100],
+              ),
+              Container(
+                color: Colors.indigo[500],
+              ),
+              Container(
+               decoration: BoxDecoration(
+                 image: DecorationImage(image: AssetImage('assets/flower-unsplash.jpg'),
+                 fit: BoxFit.cover),),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Welcome to'),
+                    Text('WellBeing',)
+                  ],
+                )
+
+              )
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
+
