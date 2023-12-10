5ccf5cc405bf3cbc70ebfb0a3409dc393aca83dc
Merin <marziabintemerin@gmail.com>
2023-12-11 01:31:44 +0600

    App keeps stopping . Old / new kono version e hoche na .

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:wellbeing_app/pages/introductionary_pages.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() => runApp( MyApp() );
+Future <void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp() ;
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
