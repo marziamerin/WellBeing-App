e056caadc55488671061b929d18d96ec24c59bd4
Merin <marziabintemerin@gmail.com>
2023-12-14 23:02:50 +0600

    Finished Sign Up kind of

--- lib/authenticate/sign up.dart
+++ lib/authenticate/sign up.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:wellbeing_app/authenticate/sign%20in.dart';
 import 'package:wellbeing_app/home/home.dart';
+
 
 class SignUp extends StatefulWidget {
   const SignUp({super.key});
@@ -10,35 +12,83 @@ class SignUp extends StatefulWidget {
 class _SignUpState extends State<SignUp> {
   String username ='';
   String password ='';
+  String email ='';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        margin: EdgeInsets.all(30.0),
-        color: Colors.white ,
         child: Form(
-          child: SafeArea(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text('Sign Up',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    decorationColor: Colors.black,
-                    fontSize: 30.0 ,
-                    fontWeight: FontWeight.bold ,
-                    letterSpacing: 1.0,
-                    fontFamily: 'PlayfairDisplay' ,
-                  ),),
-                SizedBox(
-                  height: 30.0,
-                ),
-                SizedBox(
-                  height: 18.0,
-                ) ,
-                TextFormField(
-                  decoration: InputDecoration(
-                      hintText: '  Username',
+          child: Container(
+            margin: EdgeInsets.all(30.0),
+            child: SafeArea(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('Sign Up',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      decorationColor: Colors.black,
+                      fontSize: 30.0 ,
+                      fontWeight: FontWeight.bold ,
+                      letterSpacing: 1.0,
+                      fontFamily: 'PlayfairDisplay' ,
+                    ),),
+                  SizedBox(
+                    height: 30.0,
+                  ),
+                  SizedBox(
+                    height: 14.0,
+                  ) ,
+                  TextFormField(
+                    decoration: InputDecoration(
+                        hintText: '  Username',
+                        fillColor: Colors.black12 ,
+                        filled: true ,
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(15.0)),
+                          borderSide: BorderSide(color: Colors.black12, width: 2.0),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(15.0)),
+                          borderSide: BorderSide(color: Colors.indigo, width: 2.0),
+                        )
+                    ),
+                    onChanged: (val) {
+                      setState(() => username = val);
+                    },
+                  ),
+                  SizedBox(
+                    height: 18.0),
+                  SizedBox(
+                    height: 14.0,
+                  ) ,
+                  TextFormField(
+                    decoration: InputDecoration(
+                        hintText: ' Email',
+                        fillColor: Colors.black12 ,
+                        filled: true ,
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(15.0)),
+                          borderSide: BorderSide(color: Colors.black12, width: 2.0),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(15.0)),
+                          borderSide: BorderSide(color: Colors.indigo, width: 2.0),
+                        )
+                    ),
+                    onChanged: (val) {
+                      setState(() => email = val);
+                    },
+                  ),
+                  SizedBox(
+                    height: 18.0,
+                  ),
+                  SizedBox(
+                    height: 14.0,
+                  ) ,
+                  TextFormField(
+                    decoration: InputDecoration(
+                      hintText: '  Password',
                       fillColor: Colors.black12 ,
                       filled: true ,
                       enabledBorder: OutlineInputBorder(
@@ -47,65 +97,60 @@ class _SignUpState extends State<SignUp> {
                       ),
                       focusedBorder: OutlineInputBorder(
                         borderRadius: BorderRadius.all(Radius.circular(15.0)),
-                        borderSide: BorderSide(color: Colors.deepPurple, width: 2.0),
+                        borderSide: BorderSide(color: Colors.indigo, width: 2.0),
+                      ),
+                    ),
+                    obscureText: true,
+                    onChanged: (val) {
+                      setState(() => password = val);
+                    },
+                  ),
+                  SizedBox(
+                    height: 40.0,
+                  ),
+                  ElevatedButton(onPressed: () async {
+                    print(username);
+                    print(password);
+                    Navigator.push(context, MaterialPageRoute(builder: (context) => Home()));
+                  },
+                    child: Text('Sign Up',
+                      style: TextStyle(
+                        color: Colors.white ,fontSize: 25 ,
+                        fontWeight: FontWeight.bold,
+                      ),),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.indigo[500],
+                      fixedSize: const Size(200, 50) ,
+                      animationDuration: const Duration(milliseconds: 1),
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+                    ),) ,
+                  SizedBox(
+                    height: 40.0,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text("Already have an account?",
+                        style: TextStyle(
+                          fontSize: 18,
+                        ),),
+                      SizedBox( width: 5,),
+                      GestureDetector(
+                        onTap: (){
+                          Navigator.push(context, MaterialPageRoute(builder: (context) => SignIn()));
+                        },
+                        child: Text('Log In',
+                          style: TextStyle(
+                            color: Colors.indigo[500],
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                          ),),
                       )
-                  ),
-                  onChanged: (val) {
-                    setState(() => username = val);
-                  },
-                ),
-                SizedBox(
-                  height: 20.0,
-                ),
-                SizedBox(
-                  height: 18.0,
-                ) ,
-                TextFormField(
-                  decoration: InputDecoration(
-                    hintText: '  Password',
-                    fillColor: Colors.black12 ,
-                    filled: true ,
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(15.0)),
-                      borderSide: BorderSide(color: Colors.black12, width: 2.0),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(15.0)),
-                      borderSide: BorderSide(color: Colors.deepPurple, width: 2.0),
-                    ),
-                  ),
-                  obscureText: true,
-                  onChanged: (val) {
-                    setState(() => password = val);
-                  },
-                ),
-                SizedBox(
-                  height: 40.0,
-                ),
-                ElevatedButton(onPressed: () async {
-                  print(username);
-                  print(password);
-                  Navigator.push(context, MaterialPageRoute(builder: (context) => Home()));
-                },
-                  child: Text('Log In',
-                    style: TextStyle(
-                      color: Colors.white ,fontSize: 25 ,
-                      fontWeight: FontWeight.bold,
-                    ),),
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.deepPurple[400],
-                    fixedSize: const Size(200, 50) ,
-                    animationDuration: const Duration(milliseconds: 1),
-                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
-                  ),) ,
-                SizedBox(
-                  height: 40.0,
-                ),
-                SizedBox(
-                  height: 40.0,
-                  child: Text("Don't have an account?"),
-                )
-              ],
+
+                    ],
+                  )
+                ],
+              ),
             ),
           ),
         ),
