8ef2273f2afc08fa7f3d19fe715e9a4720b34cdf
Merin <marziabintemerin@gmail.com>
2023-12-15 02:12:19 +0600

    finally <3

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -1,12 +1,24 @@
+import 'package:curved_labeled_navigation_bar/curved_navigation_bar_item.dart';
 import 'package:flutter/material.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder(
-      child: Text('Home'),
+    return Scaffold(
+      backgroundColor: Colors.deepPurple,
+      bottomNavigationBar: CurvedNavigationBar(
+        backgroundColor: Colors.deepPurple,
+        animationDuration: Duration(milliseconds: 250),
+        items: [
+          Icon(Icons.home),
+          Icon(Icons.psychology_alt),
+          Icon(Icons.diversity_1),
+          Icon(Icons.account_circle_rounded)
+        ],
+      ),
     );
   }
 }
