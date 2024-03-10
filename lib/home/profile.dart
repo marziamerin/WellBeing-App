06c3e05da2c17155efbdc7bd9a5c926ad3860c2a
Merin <marziabintemerin@gmail.com>
2024-03-10 22:30:31 +0600

    I'm back and edited the profile part .

--- lib/home/profile.dart
+++ lib/home/profile.dart
@@ -16,33 +16,92 @@ class ProfilePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.deepPurple,
-      body: Container(
-        alignment: Alignment.center,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              child: ElevatedButton(
-                onPressed: _signOut,
-                child: Text(
-                  'Log Out',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 25,
-                    fontWeight: FontWeight.bold,
-                  ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Container(
+          alignment: Alignment.center,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage('assets/profile.png'),// Add your image asset path
+              ),
+              SizedBox(height: 20),
+              Text(
+                'User',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
                 ),
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.white,
-                  fixedSize: const Size(200, 50),
-                  animationDuration: const Duration(milliseconds: 1),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30),
+              ),
+              SizedBox(height: 10),
+              Text(
+                'Patient',
+                style: TextStyle(
+                  fontSize: 18,
+                  color: Colors.white,
+                ),
+              ),
+              SizedBox(height: 20),
+              Text(
+                'I believe in the power of positive thinking and self-care. Let\'s break the stigma!',
+                textAlign: TextAlign.start,
+                style: TextStyle(
+                fontSize: 16,
+                color: Colors.white,
+              ),
+              ),
+              SizedBox(height: 20),
+              ListTile(
+                title: Text('Edit Profile',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.white,
+                  ),),
+                leading: Icon(Icons.edit_outlined ,
+                color: Colors.white),
+                onTap: () {
+                  // Navigate to the edit profile page
+                  // You can use Navigator.push for navigation
+                },
+              ),
+              ListTile(
+                title: Text('Notification Settings',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.white,
+                  ),),
+                leading: Icon(Icons.notifications_on_outlined ,
+                  color: Colors.white ,),
+                onTap: () {
+                  // Navigate to the notification settings page
+                },
+              ),
+              SizedBox( height: 10),
+              Container(
+                child: ElevatedButton(
+                  onPressed: _signOut,
+                  child: Text(
+                    'Log Out',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.white,
+                    fixedSize: const Size(150, 40),
+                    animationDuration: const Duration(milliseconds: 1),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    ),
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
